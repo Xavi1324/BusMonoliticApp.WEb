5f43e4231a95a043f3f5dd3a9b85afb1cd76f41b
Xavier Casilla <casillaxavier13@gmail.com>
2024-06-01 22:31:15 -0400

    Creando controlador

--- Data/Context/BoletosBusContext.cs
+++ Data/Context/BoletosBusContext.cs
@@ -1,9 +1,32 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BusMonoliticApp.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace BusMonoliticApp.Web.Data.Context
 {
     public class BoletosBusContext : DbContext
     {
+        #region "Constructor"
+        public BoletosBusContext(DbContextOptions<BoletosBusContext> options) : base(options)
+        {
+        }
+        #endregion
+        #region "Db Sets"
+         public DbSet<Asiento> Asiento { get; set; }
+        public DbSet<Bus> Bus { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<DetallePedido> DetallePedido { get; set; }
+        public DbSet<Empleado> Empleado { get; set; }  
+        public DbSet<Factura> Factura {get; set;}
+        public DbSet<Menu> Menu {get; set;}
+        public DbSet<Mesa> Mesa {get; set;}
+        public DbSet<Pedido> Pedido {get; set;} 
+        public DbSet<Reserva> Reserva { get; set; }
+        public DbSet<ReservaDetalle> ReservaDetalle {get; set;}
+        public DbSet<Ruta> Ruta {get; set;}
+        public DbSet<Usuario> Usuario {get; set;}
+        public DbSet<Viaje> Viaje {get; set;}
         
+        #endregion
+       
     }
 }
